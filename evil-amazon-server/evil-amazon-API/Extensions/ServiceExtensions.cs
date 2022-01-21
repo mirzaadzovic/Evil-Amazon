88f583548abad171ffe3c5ba18d8b648685ae179
Mirza <katarza.21@gmail.com>
2022-01-22 00:01:33 +0100

    cors policy added

--- evil-amazon-server/evil-amazon-API/Extensions/ServiceExtensions.cs
+++ evil-amazon-server/evil-amazon-API/Extensions/ServiceExtensions.cs
@@ -25,5 +25,19 @@ namespace API.Extensions
         {
             services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
         }
+
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                );
+            });
+            
+        }
     }
 }
--- evil-amazon-server/evil-amazon-API/Startup.cs
+++ evil-amazon-server/evil-amazon-API/Startup.cs
@@ -36,6 +36,7 @@ namespace API
 
             services.ConfigureSqlServerContext(Configuration);
             services.ConfigureRepositoryWrapper();
+            services.ConfigureCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
